273cb10cc3540218942511d12c31523a7f5f7702
Echester97 <153770376+Echester97@users.noreply.github.com>
2024-10-18 14:11:19 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/2_Расчет_количества_книг_на_дискете.py
@@ -0,0 +1,10 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+disket_volume_MB = 1.44
+disket_volume_bytes = disket_volume_MB*(1024**2)
+bytes_for_symbol = 4
+pages_by_book = 100
+rows_by_page = 50
+symbols_by_row = 25
+qty_of_symbols = pages_by_book*rows_by_page*symbols_by_row
+qty_of_books = int((disket_volume_bytes)//(qty_of_symbols*bytes_for_symbol))
+print("Количество книг, помещающихся на дискету:", qty_of_books)
