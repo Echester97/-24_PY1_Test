7ead18ebdc1248d659d89c90204e04b41100cc3c
Echester97 <153770376+Echester97@users.noreply.github.com>
2024-10-21 15:52:34 +0300

    Update 1_Замена_пропущенного_элемента_средним_арифметическим.py

--- Лабораторная работа 1/1_Замена_пропущенного_элемента_средним_арифметическим.py
+++ Лабораторная работа 1/1_Замена_пропущенного_элемента_средним_арифметическим.py
@@ -1,6 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 # TODO заменить значение пропущенного элемента средним арифметическим
-total_numbers = sum(numbers[0:4]+numbers[5:])
-count_numbers = len(numbers)
-numbers[4] = total_numbers/count_numbers
+missing_value = numbers.index(None)
+sum_of_values = sum(num for num in numbers if num is not None)
+count_of_numbers = len(numbers)
+average_value = sum_of_values/count_of_numbers
+numbers[missing_value] = average_value
 print("Измененный список:", numbers)
